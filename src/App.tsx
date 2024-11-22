5525aabb35ec891bc379f3de3448e30b2ec97b40
Nimira43 <n.rabone72@aol.com>
2024-11-22 18:35:41 +0000

    Popup component template brought in

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,24 @@ const App: Component = () => {
                 </div>
                 {/* PROFILE MENU */}
                 <div class='flex-it my-3 hover:cursor-pointer'>
-                  {/* <UserSettingPopup /> */}
+                  {/* POPUP START*/}
+                  <div class="flex-it items-center flex-row p-3 rounded-3xl hover:bg-gray-800 hover:rounded-3xl transition duration-200 cursor-pointer">
+                    <div class="flex-it">
+                      <div class="w-10 h-10 overflow-visible">
+                        <img
+                          class="rounded-full"
+                          src="https://www.pinclipart.com/picdir/middle/133-1331433_free-user-avatar-icons-happy-flat-design-png.png"
+                        ></img>
+                      </div>
+                    </div>
+                    <div class="flex-it xl:flex hidden flex-grow flex-row justify-between items-center">
+                      <div class="flex-it mx-3 font-bold">Filip99</div>
+                      <div class="flex-it">
+                        <FiMoreHorizontal />
+                      </div>
+                    </div>
+                  </div>
+                  {/* POPUP END */}
                 </div>
               </div>
             </div>
